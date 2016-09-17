d5f30c646689c5caf71db730303c719768d52773
michael_schwarz <michael.schwarz93@gmail.com>
2016-09-17 12:31:22 -0400

    Added function to add new student

--- app/scripts/teacher.js
+++ app/scripts/teacher.js
@@ -4,17 +4,30 @@ function updateStudents(teacher){
     for (var key in students) {
         if (students.hasOwnProperty(key)) {
             if(students[key]){
-                addStudent(key);
+                addStudentToList(key);
             }
         }
     }
 }
 
-function addStudent(id){
+function addStudentToList(id){
     return firebase.database().ref('/Students/' + id).once('value').then(function(snapshot) {
         console.log(snapshot.val());
 
         $("#students").append('<li>'+ snapshot.val().Name + '</li>');
 
     });
+}
+
+function addNewStudent(name) {
+    var userId = firebase.auth().currentUser.uid;
+
+    var newKey = firebase.database().ref('Students').push().key;
+
+    var updates = {};
+    updates['/Students/' + newKey] = {"Name" : name, "Teacher" : userId};
+    updates['/Teachers/' + userId + '/Students/' + newKey] = true;
+
+    return firebase.database().ref().update(updates);
+
 }
