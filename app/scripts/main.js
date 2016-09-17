9dbc04a5ec2b09a977a88a1fb0f3cf76c8ddec84
Rithu Chandrasekar <crithu@gmail.com>
2016-09-17 04:58:32 -0400

    Added Landing page buttons and login buttons

---
+++ app/scripts/main.js
@@ -0,0 +1,26 @@
+var config = {
+    apiKey: "AIzaSyD-WUhW3_jInlUPmMxV7EInTxx17C0Ba74",
+    authDomain: "pointsandme.firebaseapp.com",
+    databaseURL: "https://pointsandme.firebaseio.com",
+    storageBucket: "pointsandme.appspot.com",
+    messagingSenderId: "48621216513"
+};
+firebase.initializeApp(config);
+
+$("#teacher").bind("click", function(event){
+   $(".login").show();
+   $("#teacher").hide();
+   $("#student").hide();
+});
+
+$("#student").bind("click", function(event){
+    $(".login").show();
+    $("#teacher").hide();
+    $("#student").hide();
+});
+
+$("#backToHome").bind("click", function(event) {
+    $(".login").hide();
+    $("#teacher").show();
+    $("#student").show();
+});
