d0b3652f633052da8ed84d3e4ee39ff82b98a645
michael_schwarz <michael.schwarz93@gmail.com>
2016-09-18 06:10:15 -0400

    Getting data from controller in frontend done

--- app/controllers/teacherController.js
+++ app/controllers/teacherController.js
@@ -1,58 +1,115 @@
-app.controller('teacherCtrl', function($scope, $timeout, $window, $location) {
-	$scope.Name = null;
-	$scope.Points = null;
-	$scope.Email = null;
-	$scope.Stars = null;
+app.controller('teacherCtrl', function ($scope, $timeout, $window, $location) {
+    $scope.Name = null;
+    $scope.Points = null;
+    $scope.Email = null;
+    $scope.Stars = null;
 
 
-	if (!firebase.auth().currentUser) {
-		$window.location.href = "/views";
-		return;
-	}
+    if (!firebase.auth().currentUser) {
+        $window.location.href = "/views";
+        return;
+    }
 
-	var userId = firebase.auth().currentUser.uid;
+    var userId = firebase.auth().currentUser.uid;
+
+    firebase.database().ref('/Teachers/' + userId).once('value').then(function(snapshot) {
+        var teacher = snapshot.val();
+        var name = teacher.Name;
+
+        $timeout(function() {
+            $scope.Name = name;
+        });
+    });
+
+    firebase.database().ref('/Teachers/' + userId + '/Students').once('value').then(function (snapshot) {
+        var theirs = snapshot.val();
+
+        $timeout(function () {
+            $scope.Name = snapshot.val().Name;
+            $scope.Points = snapshot.val().Points;
+
+            firebase.database().ref("/Students").once('value').then(function (snapshot) {
+                console.log(snapshot.val());
+                var students = snapshot.val();
+
+                var student_list = [];
+
+                firebase.database().ref("/AchievementsObtained").once('value').then(function (snapshot) {
+
+                    var achievements = snapshot.val();
 
 
-	firebase.database().ref('/Teachers/' + userId + '/Students').once('value').then(function(snapshot) {
-			var theirs = snapshot.val();
+                    for (var key in students) {
+                        if (theirs.hasOwnProperty(key)) {
 
-			$timeout(function() {
-					$scope.Name = snapshot.val().Name;
-					$scope.Points = snapshot.val().Points;
+                            var stars = 0;
+                            for (var k in achievements[key]) {
+                                stars++;
+                            }
 
-					firebase.database().ref("/Students").once('value').then(function(snapshot) {
-						console.log(snapshot.val());
-						var students = snapshot.val();
+                            student_list.push({
+                                "Name": students[key].Name,
+                                "Points": students[key].Points,
+                                "Email": "email@email.org",
+                                "Stars": stars
+                            });
+                        }
+                    }
 
-						var student_list = [];
+                    $timeout(function () {
+                        $scope.Students = student_list;
+                    });
+                });
+            });
+        });
+    });
 
-						firebase.database().ref("/AchievementsObtained").once('value').then(function(snapshot){
 
-						var achievements = snapshot.val();
+    firebase.database().ref('/Rewards/' + userId).once('value').then(function (snapshot) {
+        var rewards = snapshot.val();
 
 
-						for (var key in students) {
-							if (theirs.hasOwnProperty(key)) {
+        var reward_list = [];
+        for (var key in rewards) {
+            if (rewards.hasOwnProperty(key)) {
+                reward_list.push({
+                    "Name": rewards[key].Name,
+                    "Points": rewards[key].Points,
+                });
+            }
 
-								var stars = 0;
-								for(var k in achievements[key]){
-									stars++;
-								}
+        }
 
-								student_list.push({
-									"Name": students[key].Name,
-									"Points": students[key].Points,
-									"Email" : "email@email.org",
-									"Stars" : stars
-								});
-							}
-						}
+        $timeout(function () {
+            $scope.Rewards = reward_list;
+        });
+    });
 
-						$timeout(function() {
-							$scope.Students = student_list;
-						});
-					});
-					});
-			});
-	});
+    firebase.database().ref('/Achievements/' + userId).once('value').then(function (snapshot) {
+        var achievements = snapshot.val();
+
+        var achievement_list = [];
+        for (var key in achievements) {
+            if (achievements.hasOwnProperty(key)) {
+                achievement_list.push({
+                    "Name": achievements[key].Name,
+                    "Description": achievements[key].Description,
+                });
+            }
+
+        }
+
+        $timeout(function () {
+            $scope.Achievements = achievement_list;
+        });
+    });
+
+    $scope.logout = function() {
+        firebase.auth().signOut().then(function() {
+            $window.location.href = "/views";
+        }, function(error) {
+            alert(error);
+        });
+    };
+
 });
